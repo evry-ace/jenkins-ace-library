b6f12fd3d0c6f52bf245fd03ee0fbef05ecc9ef2
Hans Kristian Flaatten <hans.flaatten@evry.com>
2019-01-21 12:39:42 +0100

    Update WORKSPACE environment after updating Jenkins workspace

--- vars/buildWorkspace.groovy
+++ vars/buildWorkspace.groovy
@@ -4,8 +4,10 @@ def call(Map opts = [:], body) {
   def workspace = opts.workspace ?: '/home/jenkins/workspace'
   def jobName = env.JOB_NAME.replaceAll(/[^A-Za-z0-9]+/, '-')
   def buildNumber = env.BUILD_NUMBER
+  def dir = "${workspace}/${jobName}-${buildNumber}"
 
-  ws(dir: "${workspace}/${jobName}-${buildNumber}") {
+  ws(dir: dir) {
+    body.env.WORKSPACE = dir
     body()
   }
 }
