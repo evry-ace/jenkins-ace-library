713cbccf17d001fb4d971e3004fccf6d0ff805b2
Endre Karlson <endre.karlson@gmail.com>
2019-04-25 13:35:43 +0200

    Make credentials non-env specific by default (#19)
    Lots of credentials like client_id and so on are non-env specific
    and therefore doesnt need to be prefixed with the env name

--- vars/envCredentials.groovy
+++ vars/envCredentials.groovy
@@ -7,7 +7,9 @@ void call(String env, List creds, Map opts, Object body) {
   List credentials = []
 
   creds.eachWithIndex { cred, index ->
-    String id = "${env}_${cred.id}"
+    Boolean prependEnv = cred.prependEnv ?: false
+    String id = prependEnv ? "${env}_${cred.id}" : cred.id
+
     String var = cred.env ? cred.env : "${prefix}${cred.id}"
     credentials.add(string(credentialsId: id, variable: var))
   }
