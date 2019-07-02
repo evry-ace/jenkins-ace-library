86737d474630c879aac94de5e26891f09334315f
Endre Karlson <endre.karlson@gmail.com>
2019-07-03 00:50:38 +0200

    Don't void

--- vars/aceContainer.groovy
+++ vars/aceContainer.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 @SuppressWarnings(['UnusedMethodParameter'])
-void call(String image, List args = [], Map opts = [:], Object body) {
+Object call(String image, List args = [], Map opts = [:], Object body) {
   if (docker) {
     docker.image(image).inside(args.join('')) {
       body()
