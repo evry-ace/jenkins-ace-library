88f7f4929b8c11aabb137dcb4d4095dd5e9f4f01
Magne Skjeret <magne.skjeret@gmail.com>
2019-07-09 09:59:23 +0200

    Update acePush.groovy

--- vars/acePush.groovy
+++ vars/acePush.groovy
@@ -11,8 +11,6 @@ void call(Map config, String env, Object image, Map opts = [:]) {
   String name = ace.helm.image
 
   println 'Pushing to Docker Registry'
-  println ace
-  println registry
 
   withDockerRegistry([credentialsId: registry, url: "https://${registry}"]) {
     println "image=${image}, imageName=${image.imageName()}, imageId=${image.id}"
