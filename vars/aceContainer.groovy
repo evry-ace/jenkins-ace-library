5264e769826e5090e883124e36a663e8b7fa98a3
Endre Karlson <endre.karlson@gmail.com>
2019-07-31 13:57:49 +0200

    Fix docker detection

--- vars/aceContainer.groovy
+++ vars/aceContainer.groovy
@@ -1,10 +1,20 @@
 #!/usr/bin/env groovy
 
+Boolean hasDocker() {
+  if (docker) {
+    if (fileExists('/var/run/docker.sock')) {
+      return true
+    } else if (env.DOCKER_HOST) {
+      return true
+    }
+  }
+
+  return false
+}
+
 @SuppressWarnings(['UnusedMethodParameter'])
 Object call(String image, List args = [], Map opts = [:], Object body) {
-  Boolean hasDocker = docker && fileExists('/var/run/docker.sock')
-
-  if (hasDocker) {
+  if (hasDocker()) {
     docker.image(image).inside(args.join('')) {
       body()
     }
