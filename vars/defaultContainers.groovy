0167cd31cc722fa9cfd489492a9937c55dae9021
Endre Karlson <endre.karlson@gmail.com>
2020-02-04 17:07:27 +0100

    Add kaniko container to defaults

--- vars/defaultContainers.groovy
+++ vars/defaultContainers.groovy
@@ -8,6 +8,7 @@ Map call() {
     cd: cdImg,
     ace: 'evryace/ace-2-values:15',
     jenkins: 'jenkins/jnlp-slave:alpine',
+    kaniko: 'gcr.io/kaniko-project/executor:debug',
   ]
 
   return containers
