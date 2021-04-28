e8a863d746f2f4bbeec7be6fbb4096563b94e803
Bjørge Solli <bjorge.solli@evry.com>
2021-04-28 09:56:35 +0200

    pull chart with correct values. creating v2 as this is a breaking change

---
+++ vars/helmPullChart2.groovy
@@ -0,0 +1,36 @@
+void call(Map target, Map opts = [:]) {
+  Map containers = opts.containers ?: [:]
+  String helmContainer = containers.helm ?: ''
+  List<String> helmOpts = opts.helmOpts ?: ["--entrypoint=''"]
+  String chart = target.chart
+  String repo = target.repo ?: https://evry-ace.github.io/helm-charts
+  String repoName = target.repoName ?: ace
+  String version = target.version ?: ''
+
+  if (!chart) {
+    error('[ace] No chart specified, dying.')
+  }
+
+  println "[ace] - Got containers - ${containers}"
+
+  aceContainerWrapper(helmContainer, helmOpts, [:]) {
+    sh """
+      set -u
+      set -e
+
+      rm -rf chart
+      mkdir chart
+
+      # Set Helm Home
+      export HELM_HOME=\$(pwd)
+      export XDG_CACHE_HOME=\$HELM_HOME/cache
+      export XDG_CONFIG_HOME=\$HELM_HOME/config
+      export XDG_data_HOME=\$HELM_HOME/data
+      helm repo add ${repoName} ${repo}
+      helm repo update
+
+      helm pull ${chart} --version "${version}" --untar -d chart
+      mv chart/${chart.split('/')[1]}/* target-data
+    """
+  }
+}
